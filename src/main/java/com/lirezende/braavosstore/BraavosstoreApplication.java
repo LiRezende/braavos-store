2f34a2c628aafc51d3f5a2f60e4544eff71555da
LiRezende <ice_lrezende@uolinc.com>
2022-01-30 18:06:01 -0300

    Backend created.

---
+++ src/main/java/com/lirezende/braavosstore/BraavosstoreApplication.java
@@ -0,0 +1,13 @@
+package com.lirezende.braavosstore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BraavosstoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BraavosstoreApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/lirezende/braavosstore/BraavosstoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lirezende.braavosstore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BraavosstoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
