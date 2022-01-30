78b325d5d19a3a94350a67b327eee2bce36517d0
LiRezende <ice_lrezende@uolinc.com>
2022-01-30 20:03:10 -0300

    Entity category created.

---
+++ src/main/java/com/lirezende/braavosstore/entities/Category.java
@@ -0,0 +1,49 @@
+package com.lirezende.braavosstore.entities;
+
+import java.io.Serializable;
+
+public class Category implements Serializable {
+    private Long id;
+    private String name;
+
+    public Category() {}
+
+    public Category(Long id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Category category = (Category) o;
+
+        if (id != null ? !id.equals(category.id) : category.id != null) return false;
+        return name != null ? name.equals(category.name) : category.name == null;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = id != null ? id.hashCode() : 0;
+        result = 31 * result + (name != null ? name.hashCode() : 0);
+        return result;
+    }
+}
