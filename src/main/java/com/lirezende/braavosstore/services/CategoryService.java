ca8b97ceecda9dca88cfc077f8c2ab0de20e249b
LiRezende <ice_lrezende@uolinc.com>
2022-01-31 13:35:58 -0300

    Database seeding.

--- src/main/java/com/lirezende/braavosstore/services/CategoryService.java
+++ src/main/java/com/lirezende/braavosstore/services/CategoryService.java
@@ -4,6 +4,7 @@ import com.lirezende.braavosstore.entities.Category;
 import com.lirezende.braavosstore.repositories.CategoryRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
@@ -12,8 +13,10 @@ public class CategoryService {
 
     @Autowired
     private CategoryRepository repository;
-     public List<Category> findAll() {
+
+    @Transactional(readOnly = true)
+    public List<Category> findAll() {
          return repository.findAll();
-     }
+    }
 
 }
