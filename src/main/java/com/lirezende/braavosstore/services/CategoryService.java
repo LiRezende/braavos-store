4d5de9247b67cae5803c36e2626b3741c4e7720e
LiRezende <ice_lrezende@uolinc.com>
2022-02-01 14:44:09 -0300

    Post Category method created.

--- src/main/java/com/lirezende/braavosstore/services/CategoryService.java
+++ src/main/java/com/lirezende/braavosstore/services/CategoryService.java
@@ -30,4 +30,12 @@ public class CategoryService {
         Category entity = obj.orElseThrow(() -> new EntityNotFoundException("Não existem resultados para esta busca."));
         return new CategoryDTO(entity);
     }
+
+    @Transactional
+    public CategoryDTO insert(CategoryDTO dto) {
+        Category entity = new Category();
+        entity.setName(dto.getName());
+        entity = repository.save(entity);
+        return new CategoryDTO(entity);
+    }
 }
