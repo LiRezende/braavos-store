4a8f020c3f96a6c3e5814516388dd1e662e87dbb
LiRezende <ice_lrezende@uolinc.com>
2022-02-08 14:46:51 -0300

    Client Repository created.

--- src/main/java/com/lirezende/braavosstore/repositories/ClientRepository.java
+++ src/main/java/com/lirezende/braavosstore/repositories/ClientRepository.java
@@ -1,9 +1,9 @@
 package com.lirezende.braavosstore.repositories;
 
-import com.lirezende.braavosstore.entities.Category;
+import com.lirezende.braavosstore.entities.Client;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface CategoryRepository extends JpaRepository<Category, Long> {
+public interface ClientRepository extends JpaRepository<Client, Long> {
 }
