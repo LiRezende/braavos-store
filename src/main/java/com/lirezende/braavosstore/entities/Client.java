10e33f33e13f7ab792c2bfa30efccbeda538a902
LiRezende <ice_lrezende@uolinc.com>
2022-02-08 14:42:24 -0300

    Client Entity updated with Getters and Setters.

--- src/main/java/com/lirezende/braavosstore/entities/Client.java
+++ src/main/java/com/lirezende/braavosstore/entities/Client.java
@@ -30,6 +30,54 @@ public class Client implements Serializable {
         this.children = children;
     }
 
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public Double getIncome() {
+        return income;
+    }
+
+    public void setIncome(Double income) {
+        this.income = income;
+    }
+
+    public Instant getBirthDate() {
+        return birthDate;
+    }
+
+    public void setBirthDate(Instant birthDate) {
+        this.birthDate = birthDate;
+    }
+
+    public Integer getChildren() {
+        return children;
+    }
+
+    public void setChildren(Integer children) {
+        this.children = children;
+    }
+
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
