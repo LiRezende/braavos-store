5dc8287cf2ea26cd5dc197120458bac0b11881ca
LiRezende <ice_lrezende@uolinc.com>
2022-02-09 13:32:05 -0300

    Client resource created.

---
+++ src/main/java/com/lirezende/braavosstore/resources/ClientResource.java
@@ -0,0 +1,36 @@
+package com.lirezende.braavosstore.resources;
+
+import com.lirezende.braavosstore.dto.ClientDTO;
+import com.lirezende.braavosstore.services.ClientService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping(value = "/clients")
+public class ClientResource {
+
+    @Autowired
+    private ClientService service;
+
+    @GetMapping
+    public ResponseEntity<Page<ClientDTO>> findAll(
+            @RequestParam(value = "page", defaultValue = "0") Integer page,
+            @RequestParam(value = "linesPerPage", defaultValue = "20") Integer linesPerPage,
+            @RequestParam(value = "direction", defaultValue = "ASC") String direction,
+            @RequestParam(value = "orderBy", defaultValue = "name") String orderBy
+    ) {
+        PageRequest pageRequest = PageRequest.of(page, linesPerPage, Sort.Direction.valueOf(direction), orderBy);
+        Page<ClientDTO> list = service.findAllPaged(pageRequest);
+        return ResponseEntity.ok().body(list);
+    }
+
+    @GetMapping(value = "/{id}")
+    public ResponseEntity<ClientDTO> findById(@PathVariable Long id) {
+        ClientDTO dto = service.findById(id);
+        return ResponseEntity.ok().body(dto);
+    }
+}
